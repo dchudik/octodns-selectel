c24005809ace550009c5f6b9b207327f48efad05
Dmitrii Chudinov <chudinov.d@selectel.com>
2023-12-11 18:57:00 +0300

    Added support dns v2

--- octodns_selectel/__init__.py
+++ octodns_selectel/__init__.py
@@ -14,7 +14,7 @@ from octodns.provider.base import BaseProvider
 from octodns.record import Record, Update
 
 # TODO: remove __VERSION__ with the next major version release
-__version__ = __VERSION__ = '0.0.4'
+__version__ = __VERSION__ = '1.0.0'
 
 
 def require_root_domain(fqdn):
@@ -41,7 +41,7 @@ class SelectelProvider(BaseProvider):
 
     PAGINATION_LIMIT = 50
 
-    API_URL = 'https://api.selectel.ru/domains/v1'
+    API_URL = 'https://api.selectel.ru/domains/v2'
 
     def __init__(self, id, token, *args, **kwargs):
         self.log = getLogger(f'SelectelProvider[{id}]')
@@ -51,14 +51,13 @@ class SelectelProvider(BaseProvider):
         self._sess = Session()
         self._sess.headers.update(
             {
-                'X-Token': token,
+                'X-Auth-Token': token,
                 'Content-Type': 'application/json',
                 'User-Agent': f'octodns/{octodns_version} octodns-selectel/{__VERSION__}',
             }
         )
-        self._zone_records = {}
-        self._domain_list = self.domain_list()
-        self._zones = None
+        self._zone_rrsets = {}
+        self._zone_list = self.zone_list()
 
     def _request(self, method, path, params=None, data=None):
         self.log.debug('_request: method=%s, path=%s', method, path)
@@ -76,21 +75,26 @@ class SelectelProvider(BaseProvider):
             return {}
         return resp.json()
 
-    def _get_total_count(self, path):
-        url = f'{self.API_URL}{path}'
-        resp = self._sess.request('HEAD', url)
-        return int(resp.headers['X-Total-Count'])
-
-    def _request_with_pagination(self, path, total_count):
+    def _request_with_pagination(self, path):
         result = []
-        for offset in range(0, total_count, self.PAGINATION_LIMIT):
-            result += self._request(
-                'GET',
-                path,
-                params={'limit': self.PAGINATION_LIMIT, 'offset': offset},
-            )
+        resp = self._request_for_pagination(path)
+        result += resp["result"]
+        offset = resp["next_offset"]
+        while offset != 0:
+            resp = self._request_for_pagination(path, offset)
+            result += resp["result"]
+            offset = resp["next_offset"]
         return result
 
+    def _request_for_pagination(self, path, offset=0):
+        result = self._request(
+            'GET',
+            path,
+            params={'limit': self.PAGINATION_LIMIT, 'offset': offset},
+        )
+        return result
+
+    # TODO: Check it. Maybe not only ttl, add comment and etc
     def _include_change(self, change):
         if isinstance(change, Update):
             existing = change.existing.data
@@ -119,7 +123,8 @@ class SelectelProvider(BaseProvider):
         new = change.new
         params_for = getattr(self, f'_params_for_{new._type}')
         for params in params_for(new):
-            self.create_record(zone_name, params)
+            self.log.debug("params: %s", params_for(new))
+            self.create_rrset(zone_name, params)
 
     def _apply_update(self, zone_name, change):
         self._apply_delete(zone_name, change)
@@ -127,12 +132,11 @@ class SelectelProvider(BaseProvider):
 
     def _apply_delete(self, zone_name, change):
         existing = change.existing
-        self.delete_record(zone_name, existing._type, existing.name)
+        self.delete_rrset(zone_name, existing._type, existing.name)
 
     def _params_for_multiple(self, record):
-        for value in record.values:
             yield {
-                'content': value,
+                'records': list(map(lambda value: {'content':value, 'disabled':False}, record.values)),
                 'name': record.fqdn,
                 'ttl': max(self.MIN_TTL, record.ttl),
                 'type': record._type,
@@ -140,12 +144,12 @@ class SelectelProvider(BaseProvider):
 
     def _params_for_single(self, record):
         yield {
-            'content': record.value,
+            'records': [{'content':record.value, 'disabled':False}],
             'name': record.fqdn,
             'ttl': max(self.MIN_TTL, record.ttl),
             'type': record._type,
         }
-
+    # TODO: test it
     def _params_for_MX(self, record):
         for value in record.values:
             yield {
@@ -155,7 +159,7 @@ class SelectelProvider(BaseProvider):
                 'type': record._type,
                 'priority': value.preference,
             }
-
+    # TODO: test it
     def _params_for_SRV(self, record):
         for value in record.values:
             yield {
@@ -167,7 +171,7 @@ class SelectelProvider(BaseProvider):
                 'weight': value.weight,
                 'priority': value.priority,
             }
-
+    # TODO: test it
     def _params_for_SSHFP(self, record):
         for value in record.values:
             yield {
@@ -187,22 +191,23 @@ class SelectelProvider(BaseProvider):
     _params_for_CNAME = _params_for_single
     _params_for_ALIAS = _params_for_single
 
-    def _data_for_A(self, _type, records):
+    def _data_for_A(self, _type, rrset):
         return {
-            'ttl': records[0]['ttl'],
+            'ttl': rrset['ttl'],
             'type': _type,
-            'values': [r['content'] for r in records],
+            'values': [r['content'] for r in rrset["records"]],
         }
 
     _data_for_AAAA = _data_for_A
 
-    def _data_for_NS(self, _type, records):
+    def _data_for_NS(self, _type, rrset):
         return {
-            'ttl': records[0]['ttl'],
+            'ttl': rrset['ttl'],
             'type': _type,
-            'values': [require_root_domain(r["content"]) for r in records],
+            'values': [require_root_domain(r["content"]) for r in rrset["records"]],
         }
 
+    # TODO: test it
     def _data_for_MX(self, _type, records):
         values = []
         for record in records:
@@ -214,6 +219,7 @@ class SelectelProvider(BaseProvider):
             )
         return {'ttl': records[0]['ttl'], 'type': _type, 'values': values}
 
+    # TODO: test it
     def _data_for_CNAME(self, _type, records):
         only = records[0]
         return {
@@ -224,6 +230,7 @@ class SelectelProvider(BaseProvider):
 
     _data_for_ALIAS = _data_for_CNAME
 
+    # TODO: test it
     def _data_for_TXT(self, _type, records):
         return {
             'ttl': records[0]['ttl'],
@@ -231,6 +238,7 @@ class SelectelProvider(BaseProvider):
             'values': [r['content'] for r in records],
         }
 
+    # TODO: test it
     def _data_for_SRV(self, _type, records):
         values = []
         for record in records:
@@ -244,7 +252,8 @@ class SelectelProvider(BaseProvider):
             )
 
         return {'type': _type, 'ttl': records[0]['ttl'], 'values': values}
-
+    
+    # TODO: test it
     def _data_for_SSHFP(self, _type, records):
         values = []
         for record in records:
@@ -258,6 +267,7 @@ class SelectelProvider(BaseProvider):
 
         return {'type': _type, 'ttl': records[0]['ttl'], 'values': values}
 
+    # TODO: what is it?
     def populate(self, zone, target=False, lenient=False):
         self.log.debug(
             'populate: name=%s, target=%s, lenient=%s',
@@ -266,88 +276,90 @@ class SelectelProvider(BaseProvider):
             lenient,
         )
         before = len(zone.records)
-        records = self.zone_records(zone)
+        records = self.zone_rrsets(zone)
         if records:
             values = defaultdict(lambda: defaultdict(list))
+            self.log.debug("Values: %s", values)
             for record in records:
                 name = zone.hostname_from_fqdn(record['name'])
                 _type = record['type']
                 if _type in self.SUPPORTS:
                     values[name][record['type']].append(record)
+            self.log.debug("Values items: %s", values.items())
             for name, types in values.items():
-                for _type, records in types.items():
+                self.log.debug("Name: %s Types: %s", name, types.items())
+                for _type, rrset in types.items():
                     data_for = getattr(self, f'_data_for_{_type}')
-                    data = data_for(_type, records)
-                    record = Record.new(
-                        zone, name, data, source=self, lenient=lenient
-                    )
-                    zone.add_record(record)
+                    if rrset:
+                        data = data_for(_type, rrset[0])
+                        record = Record.new(
+                            zone, name, data, source=self, lenient=lenient
+                        )
+                        zone.add_record(record)
         self.log.info(
             'populate:   found %s records', len(zone.records) - before
         )
 
-    def domain_list(self):
-        path = '/'
-        domains = {}
-        domains_list = []
-
-        total_count = self._get_total_count(path)
-        domains_list = self._request_with_pagination(path, total_count)
-
-        for domain in domains_list:
-            domains[domain['name']] = domain
-        return domains
-
-    def zone_records(self, zone):
-        path = f'/{zone.name[:-1]}/records/'
-        zone_records = []
-
-        total_count = self._get_total_count(path)
-        zone_records = self._request_with_pagination(path, total_count)
-
-        self._zone_records[zone.name] = zone_records
-        return self._zone_records[zone.name]
-
-    def create_domain(self, name, zone=""):
-        path = '/'
-
-        data = {'name': name, 'bind_zone': zone}
-
+    def create_zone(self, name):
+        path = '/zones'
+        data = {'name': name}
         resp = self._request('POST', path, data=data)
-        self._domain_list[name] = resp
+        self._zone_list[name] = resp
+
         return resp
 
-    def create_record(self, zone_name, data):
-        self.log.debug('Create record. Zone: %s, data %s', zone_name, data)
-        if zone_name in self._domain_list.keys():
-            domain_id = self._domain_list[zone_name]['id']
-        else:
-            domain_id = self.create_domain(zone_name)['id']
+    def zone_list(self):
+        path = '/zones'
+        zones = {}
+        zones_list = self._request_with_pagination(path)
+        for zone in zones_list:
+            zones[zone['name']] = zone
 
-        path = f'/{domain_id}/records/'
+        return zones
+
+    def zone_rrsets(self, zone):
+        zone_rrsets = []
+        zone_by_name = self._zone_list.get(zone.name)
+        zone_id = zone_by_name.get("uuid") if zone_by_name else None
+        if zone_id:
+            path = f'/zones/{zone_id}/rrset'
+            zone_rrsets = self._request_with_pagination(path)
+        self._zone_rrsets[require_root_domain(zone.name)] = zone_rrsets
+        self.log.debug('View rrset. Zone: %s, data %s', zone, zone_rrsets)
+
+        return self._zone_rrsets[require_root_domain(zone.name)]
+
+    def create_rrset(self, zone_name, data):
+        self.log.debug('Create rrset. Zone: %s, data %s', zone_name, data)
+        if require_root_domain(zone_name) in self._zone_list.keys():
+            zone_id = self._zone_list[require_root_domain(zone_name)]['uuid']
+        else:
+            zone_id = self.create_zone(zone_name)['uuid']
+
+        path = f'/zones/{zone_id}/rrset'
         return self._request('POST', path, data=data)
 
-    def delete_record(self, domain, _type, zone):
-        self.log.debug('Delete records. Domain: %s, Type: %s', domain, _type)
-        domain_id = self._domain_list[domain]['id']
-        records = self._zone_records.get(f'{domain}.', False)
-        if not records:
-            path = f'/{domain_id}/records/'
-            records = self._request('GET', path)
-
-        full_domain = f'{zone}.{domain}' if zone else domain
+    def delete_rrset(self, zone, _type, rrset_name):
+        self.log.debug('Delete rrsets. Zone: %s, Type: %s, Rrset Name: %s', zone, _type, rrset_name)
+        zone_id = self._zone_list[require_root_domain(zone)]['uuid']
+        rrsets = self._zone_rrsets.get(f'{zone}.', False)
+        if not rrsets:
+            path = f'/zones/{zone_id}/rrset'
+            rrsets = self._request('GET', path)
+        # TODO: check rrset name if it fqdn (subdomain with domain)
+        full_domain = f'{rrset_name}.{require_root_domain(zone)}'
         delete_count, skip_count = 0, 0
-        for record in records:
-            if record['type'] == _type and record['name'] == full_domain:
-                record_id = record["id"]
-                path = f'/{domain_id}/records/{record_id}'
+        for rrset in rrsets:
+            if rrset['type'] == _type and rrset['name'] == full_domain:
+                rrset_id = rrset["uuid"]
+                path = f'/zones/{zone_id}/rrset/{rrset_id}'
                 try:
                     self._request('DELETE', path)
                     delete_count += 1
                 except HTTPError:
                     skip_count += 1
-                    self.log.warning(f'Failed to delete record {record_id}')
+                    self.log.warning(f'Failed to delete rrset {rrset_id}')
 
         self.log.debug(
-            f'Deleted {delete_count} records. Skipped {skip_count} records'
+            f'Deleted {delete_count} rrsets. Skipped {skip_count} rrsets'
         )
