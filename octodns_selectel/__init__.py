c2526aae98b5a656213a97358f43ca30452a31a2
Dmitrii Chudinov <chudinov.d@selectel.com>
2023-12-12 23:18:08 +0300

    Fix MX params. Fix mappings from rrset to records

--- octodns_selectel/__init__.py
+++ octodns_selectel/__init__.py
@@ -60,16 +60,17 @@ class SelectelProvider(BaseProvider):
         self._zone_list = self.zone_list()
 
     def _request(self, method, path, params=None, data=None):
-        self.log.debug('_request: method=%s, path=%s', method, path)
-
+        self.log.debug('_request: method=%s, path=%s, data=%s', method, path, data)
         url = f'{self.API_URL}{path}'
         resp = self._sess.request(method, url, params=params, json=data)
-
         self.log.debug('_request: status=%s', resp.status_code)
         if resp.status_code == 401:
             raise SelectelAuthenticationRequired(resp.text)
         elif resp.status_code == 404:
             return {}
+        # TODO: delete it. It for debug only
+        # if resp.json():
+        #     self.log.debug('_request: response=%s', resp.json())
         resp.raise_for_status()
         if method == 'DELETE':
             return {}
@@ -94,7 +95,6 @@ class SelectelProvider(BaseProvider):
         )
         return result
 
-    # TODO: Check it. Maybe not only ttl, add comment and etc
     def _include_change(self, change):
         if isinstance(change, Update):
             existing = change.existing.data
@@ -113,7 +113,6 @@ class SelectelProvider(BaseProvider):
         self.log.debug(
             '_apply: zone=%s, len(changes)=%d', desired.name, len(changes)
         )
-
         zone_name = desired.name[:-1]
         for change in changes:
             class_name = change.__class__.__name__
@@ -142,6 +141,14 @@ class SelectelProvider(BaseProvider):
             'type': record._type,
         }
 
+    def _params_for_multiple_TXT(self, record):
+        yield {
+            'records': list(map(lambda value: {'content':'"%s"'%value, 'disabled':False}, record.values)),
+            'name': record.fqdn,
+            'ttl': max(self.MIN_TTL, record.ttl),
+            'type': record._type,
+        }
+
     def _params_for_single(self, record):
         yield {
             'records': [{'content':record.value, 'disabled':False}],
@@ -149,7 +156,7 @@ class SelectelProvider(BaseProvider):
             'ttl': max(self.MIN_TTL, record.ttl),
             'type': record._type,
         }
-    # TODO: test it
+
     def _params_for_MX(self, record):
         yield {
             'records': list(map(lambda value: {'content':f'{value.preference} {value.exchange}', 'disabled':False}, record.values)),
@@ -158,7 +165,6 @@ class SelectelProvider(BaseProvider):
             'type': record._type,
         }
 
-    # TODO: test it
     def _params_for_SRV(self, record):
         yield {
                 'name': record.fqdn,
@@ -168,7 +174,7 @@ class SelectelProvider(BaseProvider):
                                     {'content':f'{value.priority} {value.weight} {value.port} {value.target}', 
                                      'disabled':False}, record.values)),
             }
-    # TODO: test it
+
     def _params_for_SSHFP(self, record):
         yield {
                 'name': record.fqdn,
@@ -180,7 +186,8 @@ class SelectelProvider(BaseProvider):
     _params_for_A = _params_for_multiple
     _params_for_AAAA = _params_for_multiple
     _params_for_NS = _params_for_multiple
-    _params_for_TXT = _params_for_multiple
+    _params_for_TXT = _params_for_multiple_TXT
+    _params_for_MX = _params_for_MX
 
     _params_for_CNAME = _params_for_single
     _params_for_ALIAS = _params_for_single
@@ -201,38 +208,37 @@ class SelectelProvider(BaseProvider):
             'values': [require_root_domain(r["content"]) for r in rrset["records"]],
         }
 
-    # TODO: test it
-    def _data_for_MX(self, _type, records):
+    def _data_for_MX(self, _type, rrset):
         values = []
-        for record in records:
-            values.append(
-                {
-                    'preference': record['priority'],
-                    'exchange': require_root_domain(record["content"]),
-                }
-            )
-        return {'ttl': records[0]['ttl'], 'type': _type, 'values': values}
+        if rrset["records"]:
+            for record in rrset["records"]:
+                priority,exchange = record["content"].split(" ")
+                values.append(
+                    {
+                        'preference': priority,
+                        'exchange': require_root_domain(exchange),
+                    }
+                )
+        return {'ttl': rrset['ttl'], 'type': _type, 'values': values}
 
-    # TODO: test it
-    def _data_for_CNAME(self, _type, records):
-        only = records[0]
+    def _data_for_CNAME(self, _type, rrset):
+        value = rrset["records"][0]["content"]
         return {
-            'ttl': only['ttl'],
+            'ttl': rrset['ttl'],
             'type': _type,
-            'value': require_root_domain(only["content"]),
+            'value': require_root_domain(value),
         }
 
     _data_for_ALIAS = _data_for_CNAME
 
-    # TODO: test it
-    def _data_for_TXT(self, _type, records):
+    def _data_for_TXT(self, _type, rrset):
         return {
-            'ttl': records[0]['ttl'],
+            'ttl': rrset['ttl'],
             'type': _type,
-            'values': [r['content'] for r in records],
+            'values': [r['content'] for r in rrset["records"]],
         }
 
-    # TODO: test it
+    # TODO: fix it
     def _data_for_SRV(self, _type, records):
         values = []
         for record in records:
@@ -247,7 +253,7 @@ class SelectelProvider(BaseProvider):
 
         return {'type': _type, 'ttl': records[0]['ttl'], 'values': values}
     
-    # TODO: test it
+    # TODO: fix it
     def _data_for_SSHFP(self, _type, records):
         values = []
         for record in records:
@@ -261,7 +267,7 @@ class SelectelProvider(BaseProvider):
 
         return {'type': _type, 'ttl': records[0]['ttl'], 'values': values}
 
-    # TODO: what is it?
+    # TODO: refactor it
     def populate(self, zone, target=False, lenient=False):
         self.log.debug(
             'populate: name=%s, target=%s, lenient=%s',
@@ -273,15 +279,12 @@ class SelectelProvider(BaseProvider):
         records = self.zone_rrsets(zone)
         if records:
             values = defaultdict(lambda: defaultdict(list))
-            self.log.debug("Values: %s", values)
             for record in records:
                 name = zone.hostname_from_fqdn(record['name'])
                 _type = record['type']
                 if _type in self.SUPPORTS:
                     values[name][record['type']].append(record)
-            self.log.debug("Values items: %s", values.items())
             for name, types in values.items():
-                self.log.debug("Name: %s Types: %s", name, types.items())
                 for _type, rrset in types.items():
                     data_for = getattr(self, f'_data_for_{_type}')
                     if rrset:
@@ -295,11 +298,11 @@ class SelectelProvider(BaseProvider):
         )
 
     def create_zone(self, name):
+        self.log.debug('Create zone: %s', name)
         path = '/zones'
         data = {'name': name}
         resp = self._request('POST', path, data=data)
         self._zone_list[name] = resp
-
         return resp
 
     def zone_list(self):
@@ -308,10 +311,10 @@ class SelectelProvider(BaseProvider):
         zones_list = self._request_with_pagination(path)
         for zone in zones_list:
             zones[zone['name']] = zone
-
         return zones
 
     def zone_rrsets(self, zone):
+        self.log.debug('View rrset. Zone: %s', zone)
         zone_rrsets = []
         zone_by_name = self._zone_list.get(zone.name)
         zone_id = zone_by_name.get("uuid") if zone_by_name else None
@@ -319,8 +322,6 @@ class SelectelProvider(BaseProvider):
             path = f'/zones/{zone_id}/rrset'
             zone_rrsets = self._request_with_pagination(path)
         self._zone_rrsets[require_root_domain(zone.name)] = zone_rrsets
-        self.log.debug('View rrset. Zone: %s, data %s', zone, zone_rrsets)
-
         return self._zone_rrsets[require_root_domain(zone.name)]
 
     def create_rrset(self, zone_name, data):
@@ -329,11 +330,11 @@ class SelectelProvider(BaseProvider):
             zone_id = self._zone_list[require_root_domain(zone_name)]['uuid']
         else:
             zone_id = self.create_zone(zone_name)['uuid']
-
         path = f'/zones/{zone_id}/rrset'
 
         return self._request('POST', path, data=data)
 
+    # TODO: refactor it
     def delete_rrset(self, zone, _type, rrset_name):
         self.log.debug('Delete rrsets. Zone: %s, Type: %s, Rrset Name: %s', zone, _type, rrset_name)
         zone_id = self._zone_list[require_root_domain(zone)]['uuid']
@@ -353,7 +354,6 @@ class SelectelProvider(BaseProvider):
                 except HTTPError:
                     skip_count += 1
                     self.log.warning(f'Failed to delete rrset {rrset_id}')
-
         self.log.debug(
             f'Deleted {delete_count} rrsets. Skipped {skip_count} rrsets'
         )
